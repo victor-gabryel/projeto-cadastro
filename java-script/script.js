e0015be84b61ff0fb416873c4cec0eb56105e05b
Victor <victorgabryel2123@gmail.com>
2024-06-09 21:14:59 -0300

    versao-1.4

--- java-script/script.js
+++ java-script/script.js
@@ -1,9 +1,17 @@
-function menu(){
+function menushow() {
+    let menumobile = document.querySelector('.mobile-menu')
 
+    if (menumobile.classList.contains('open')) {
+        menumobile.classList.remove('open')
+        document.querySelector('.icon').src = "../imagens/menu_white_36dp.svg";
+    } else {
+        menumobile.classList.add('open')
+        document.querySelector('.icon').src = "../imagens/close_white_36dp.svg";
+    }
 }
 
 
-function entrar(){
+function entrar() {
 
     let usuario = window.document.getElementById('txtusuario')
 
