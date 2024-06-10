80d7d246a5ab730cb74f53e00af5dd2dd0e9b56e
Victor <victorgabryel2123@gmail.com>
2024-06-10 13:51:54 -0300

    versao-1.4

--- java-script/script.js
+++ java-script/script.js
@@ -1,27 +1,60 @@
-function menushow() {
-    let menumobile = document.querySelector('.mobile-menu')
-
-    if (menumobile.classList.contains('open')) {
-        menumobile.classList.remove('open')
-        document.querySelector('.icon').src = "../imagens/menu_white_36dp.svg";
-    } else {
-        menumobile.classList.add('open')
-        document.querySelector('.icon').src = "../imagens/close_white_36dp.svg";
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
     }
-}
-
-
-function entrar() {
-
-    let usuario = window.document.getElementById('txtusuario')
-
-    let senha = window.document.getElementById('txtsenha')
-
-    if (usuario.value == 'Victor' && senha.value == 123){
-
-        window.location = ('')
-
-    } else {
-        window.alert('Usuário ou senha incorreto!')
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
     }
-}
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+
+
+// function entrar() {
+
+//     let usuario = window.document.getElementById('txtusuario')
+
+//     let senha = window.document.getElementById('txtsenha')
+
+//     if (usuario.value == 'Victor' && senha.value == 123){
+
+//         window.location = ('')
+
+//     } else {
+//         window.alert('Usuário ou senha incorreto!')
+//     }
+// }
